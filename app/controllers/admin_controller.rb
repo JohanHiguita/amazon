b294d00b9356d306a8d352294395984f25ed9a89
Johan Higuita <higuitajohan@gmail.com>
2018-12-07 20:38:13 -0500

    ver tablas y grafica admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,26 @@
 class AdminController < ApplicationController
 
+	before_action :authenticate_user! #viene con devise
+	before_action :is_admin?
+
 	layout "admin"
 
 	def index
-		
+		@users= User.all
+		@subscriptors=Subscriptor.all.order(created_at: :desc)
+		@posts = Post.all.order(created_at: :desc)
+		@comments = Comment.all
+	
+	end
+
+
+	private
+
+	def is_admin?
+		unless current_user.admin?
+			flash[:alert] = "You do not have permissions"
+			redirect_to root_path
+		end
 	end
 
 end
